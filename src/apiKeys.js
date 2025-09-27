7ae56e275544e09039cb6e25d51b4954dfe6c3dd
NikhilVerma09 <nikhilverma2693@gmail.com>
2025-09-27 10:34:16 +0530

    my  project

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "16132255f48eb391d840e70473d5a5ea",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
